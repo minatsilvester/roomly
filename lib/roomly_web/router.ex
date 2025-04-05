1023f063a242283ade8cce89cd77fce77016250e
Harisudhan V R <harisudhan.v.r@maersk.com>
2025-04-05 20:47:26 +0530

    Fix user return to after login

--- lib/roomly_web/router.ex
+++ lib/roomly_web/router.ex
@@ -18,7 +18,7 @@ defmodule RoomlyWeb.Router do
   end
 
   scope "/", RoomlyWeb do
-    pipe_through :browser
+    pipe_through [:browser, :require_authenticated_user]
 
     get "/", PageController, :home
 
